24098ee4e614f8967000317e76034345c2d46bca
Junli Zhang <zhjl86@gmail.com>
2019-12-25 23:15:40 -0800

    add some examples

---
+++ Examples/commands.sh
@@ -0,0 +1,7 @@
+# download the proper version of CRISgo in this folder and run the commands below to check how it works
+
+./CRISgov3 reference-example.fasta gene1-g10 gene1  gcggagaactg ccggcccggaagag GAGGCAGGCGTCGAAGAGTACGG 
+
+./CRISgov3 reference-example.fasta gene1-g14 gene1 gcggagaactg ccggcccggaagag CGGCCCTGAAGAAGACGGCGGGG
+
+./CRISgov3 reference-example.fasta gene2-g6 gene2 TCCTTTGAACAAA AGGCTTTTGTTCAG CCTGCGCCACTACTACAAACTAA
