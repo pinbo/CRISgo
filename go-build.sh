b0a49ce18133dd31b6bbc6f3354c252120de9efc
Junli Zhang <zhjl86@gmail.com>
2020-03-02 11:54:40 -0800

    add a script to build across platforms

---
+++ go-build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+package=$1
+if [[ -z "$package" ]]; then
+  echo "usage: $0 <package-name>"
+  exit 1
+fi
+
+package_name=(${package/.go/})
+
+
+platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+	echo "Building for $platform"
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
